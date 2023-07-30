c9dc4e7d2f89b4b34cb6fa29740848ac9eb9ab8b
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-07-31 00:31:50 +0530

    Deployment configuration for AWS

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -13,7 +13,7 @@ class PredictPipeline:
             # model_path='artifacts\model.pkl'
             # preprocessor_path='artifacts\proprocessor.pkl'
             model_path = os.path.join('artifacts', 'model.pkl')
-            preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+            preprocessor_path = os.path.join('artifacts', 'proprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
